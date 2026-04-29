704388b7ed9dc1826494b7e29d4b5296ad4bce06
JJ_Pedrique <110735261+JJPedrique@users.noreply.github.com>
2026-04-29 10:39:20 -0400

    Add files via upload

---
+++ src/App.java
@@ -0,0 +1,13 @@
+
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        JFrame window = new JFrame("Garita");
+        window.setSize(500,300);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setVisible(true);
+
+        System.out.println("Hello, World!");
+    }
+}
